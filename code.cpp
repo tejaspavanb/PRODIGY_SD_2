4e1c8eaf8b8d036045d90bdb4e705b7a5d05c84d
tejaspavanb <160583886+tejaspavanb@users.noreply.github.com>
2024-06-09 20:29:52 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    // Initialize random seed
+    srand(static_cast<unsigned int>(time(0)));
+
+    // Generate a random number between 1 and 100
+    int randomNumber = rand() % 100 + 1;
+    int guess;
+    int attempts = 0;
+
+    cout << "Guess the number between 1 and 100." << endl;
+
+    // Loop until the user guesses the number
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess > randomNumber) {
+            cout << "Too high! Try again." << endl;
+        } else if (guess < randomNumber) {
+            cout << "Too low! Try again." << endl;
+        } else {
+            cout << "Congratulations! You guessed the number in " << attempts << " attempts." << endl;
+        }
+    } while (guess != randomNumber);
+
+    return 0;
+}
